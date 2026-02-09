31efb64c62608101854be60ad3555b631997555f
nn-hieu <nn.hieu0701@gmail.com>
2026-02-09 15:47:22 +0700

    API description

--- file-sender/src/main/java/com/hieunn/filesender/controllers/FileController.java
+++ file-sender/src/main/java/com/hieunn/filesender/controllers/FileController.java
@@ -1,6 +1,9 @@
 package com.hieunn.filesender.controllers;
 
 import com.hieunn.filesender.services.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +20,29 @@ public class FileController {
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Void> sendFile(@RequestPart MultipartFile file, @RequestParam String targetService) {
+    @Operation(
+            summary = "API Send file",
+            description = """
+                    Send your file to another service through MQTT Broker (Mosquitto)
+                    """
+    )
+    public ResponseEntity<Void> sendFile(
+            @RequestPart
+            @Parameter(
+                    description = "Your file",
+                    required = true,
+                    content = @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+            )
+            MultipartFile file,
+
+            @RequestParam
+            @Parameter(
+                    description = "Name of the service you want to send file to",
+                    example = "file-receiver",
+                    required = true
+            )
+            String targetService
+    ) {
         fileService.sendFile(file, targetService);
 
         return ResponseEntity.ok().build();
